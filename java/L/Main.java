a96d07b9dba1651e913f53d15e004cea7b6bfdce
Mutabona <k_dod1@mail.ru>
2023-02-20 22:08:35 +0300

    sram

--- java/L/Main.java
+++ java/L/Main.java
@@ -1,7 +1,59 @@
-package L;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int k = scn.nextInt();
+        int m = scn.nextInt();
+        int n = scn.nextInt();
+        int s[][][] = new int[k][m][n];
+        int a[][][] = new int[k][m][n];
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int z = 0; z < n; z++) {
+                    s[i][j][z] = scn.nextInt();
+                }
+            }
+        }
+
+        a[0][0][0] = s[0][0][0];
+
+        for (int i = k-1; i > 0; i--) {
+            a[i][0][0] = s[i][0][0] - s[i-1][0][0];
+        }
+        for (int i = m-1; i > 0; i--) {
+            a[0][i][0] = s[0][i][0] - s[0][i-1][0];
+        }
+        for (int i = n-1; i > 0; i--) {
+            a[0][0][i] = s[0][0][i] - s[0][0][i-1];
+        }
+
+        for (int i = 1; i < k; i++) {
+            for (int j = 1; j < n; j++) {
+                a[i][0][j] = s[i][0][j] - s[i][0][j-1] - a[i-1][0][j];
+            }
+        }
+        for (int i = 1; i < k; i++) {
+            for (int j = 1; j < m; j++) {
+                a[i][j][0] = s[i][j][0] - s[i][j-1][0] - a[i-1][j][0];
+            }
+        }
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                a[0][j][i] = s[0][j][i] - s[0][j][i-1] - a[0][j-1][i];
+            }
+        }
+
+        System.out.print("\n");
+        for (int i = 0; i < k; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int z = 0; z < n; z++) {
+                    System.out.print(a[i][j][z] + " ");
+                }
+                System.out.print("\n");
+            }
+            System.out.print("\n");
+        }
         
     }
 }
