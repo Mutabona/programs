e4cf91da015f98b55c43215baa9f74c5d5732968
Mutabona <k_dod1@mail.ru>
2023-03-14 11:57:17 +0300

    er

--- java/regex/regex.java
+++ java/regex/regex.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 public class regex {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("\\d{2}");
-        int time = getNumber(pattern) + getNumber(pattern);
+        Scanner scn = new Scanner(System.in);
+        int time = 0;
+        String line;
+        while(scn.hasNextLine()) {
+            line = scn.nextLine();
+            time += getNumber(pattern, line);
+        }
         System.out.printf("%02d:%02d", (time/60)%24, (time%60));
     }
 
-    public static int getNumber(Pattern pattern) {
-        Scanner scn = new Scanner(System.in);
-        String text = scn.next();
-        Matcher matcher = pattern.matcher(text);
+    public static int getNumber(Pattern pattern, String line) {
+        Matcher matcher = pattern.matcher(line);
         matcher.find();
-        int minutes = (Integer.parseInt(text.substring(matcher.start(), matcher.end())))*60;
+        int minutes = (Integer.parseInt(line.substring(matcher.start(), matcher.end())))*60;
         matcher.find();
-        minutes += Integer.parseInt(text.substring(matcher.start(), matcher.end()));
+        minutes += Integer.parseInt(line.substring(matcher.start(), matcher.end()));
         return minutes;
     }
 }
